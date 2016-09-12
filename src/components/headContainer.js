b379a13f977ae1e3754aff99841317205bb2f1b8
James Waters <Jwaters@Jamess-MacBook-Pro.local>
2016-09-12 10:56:39 -0400

    header container created

---
+++ src/components/headContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import styles from '../sass/headContainer.scss'
+
+class HeadContainer extends React.Component{
+  render(){
+    return(
+      <div id="container">
+
+      </div>
+  )
+  }
+}
+
+
+export default HeadContainer;
--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import HeadContainer from './HeadContainer';
 
 
 class Main extends React.Component {
@@ -10,7 +11,7 @@ class Main extends React.Component {
   }
 	render() {
 		return <div>
-			<h1>Hello World</h1>
+			<HeadContainer />
 		</div>	
 	}
 };
